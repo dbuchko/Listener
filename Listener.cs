b9f9c1fb65b06533916bcd13ce512fa69e6079a8
Daniel L. Buchko <dbuchko@pivotal.io>
2018-01-19 14:37:25 -0500

    Added ability to change AMQP connection port and disable topology recovery via environment variables.  Added consumer cancellation listener.

--- Listener.cs
+++ Listener.cs
@@ -12,11 +12,16 @@ using Steeltoe.Extensions.Configuration;
 class Worker
 {
 
+    private static ConnectionFactory factory;
+    private static IConnection connection;
+    private static IModel channel;
+    private static EventingBasicConsumer consumer;
+
     public static void Main()
     {
         RabbitMqConsoleEventListener loggingEventSource = new RabbitMqConsoleEventListener();
 
-        var msgsReceived = 0;
+        int msgsReceived = 0;
 
         // Set default interval for heartbeats
         ushort heartbeatInterval = 20;
@@ -32,7 +37,13 @@ class Worker
 
         Console.WriteLine("Setting heartbeat interval to {0} s", heartbeatInterval);
 
-        var factory = new ConnectionFactory() { HostName = "localhost" };
+        int port = 5672;
+        string portStr = Environment.GetEnvironmentVariable("RABBITMQ_NODE_PORT");
+        if (portStr != null) {
+            port = Convert.ToInt32(portStr);
+        }
+
+        factory = new ConnectionFactory() { HostName = "localhost", Port = port };
         if (Environment.GetEnvironmentVariable("VCAP_SERVICES") != null) {
             // Running on PCF
             IServiceCollection services = new ServiceCollection();
@@ -50,19 +61,27 @@ class Worker
         // Since we have a durable queue anyways, there should be no need to recreate it on a connection failure.
         // Otherwise this currently can result in exceptions (if the durable queue home node is down), that
         // hangs the RMQ client.
-        factory.TopologyRecoveryEnabled = false;
+        String topologyRecoveryEnabled = Environment.GetEnvironmentVariable("TOPOLOGY_RECOVERY_ENABLED");
+        if (topologyRecoveryEnabled != null)
+        {
+            if (topologyRecoveryEnabled.ToUpper().Equals("FALSE"))
+            {
+                Console.WriteLine("Disabling topology recovery.");
+                factory.TopologyRecoveryEnabled = false;
+            }
+        }
 
         // Reduce the heartbeat interval so that bad connections are detected sooner than the default of 60s
         factory.RequestedHeartbeat = heartbeatInterval;
 
-        using (var connection = factory.CreateConnection())
-        using (var channel = connection.CreateModel())
+        using (connection = factory.CreateConnection())
+        using (channel = connection.CreateModel())
         {
             channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
             Console.WriteLine(" [*] Waiting for messages.");
 
-            var consumer = new EventingBasicConsumer(channel);
+            consumer = new EventingBasicConsumer(channel);
             consumer.Received += (model, ea) =>
             {
                 msgsReceived++;
@@ -73,6 +92,10 @@ class Worker
             {
                 Console.WriteLine("*** Entering Shutdown ***");
             };
+            consumer.ConsumerCancelled += (model, ea) =>
+            {
+                Console.WriteLine("*** Received ConsumerCancelled ***");
+            };
 
             channel.BasicConsume(queue: "task_queue", autoAck: true, consumer: consumer);
             channel.CallbackException += (sender, e) => Console.WriteLine(e.Exception);
@@ -82,5 +105,7 @@ class Worker
                 Thread.Sleep(1000);
             }
         }
+
     }
+
 }
