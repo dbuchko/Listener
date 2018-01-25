c25899d776f3de9142558eb6ad5642e055a4d187
Luke Bakken <luke@bakken.io>
2018-01-25 09:05:12 -0800

    Changes to run in my env

--- Listener.cs
+++ Listener.cs
@@ -1,22 +1,15 @@
 ﻿using System;
-using System.Text;
-using System.Threading;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using RabbitMQ.Client.Logging;
-using Steeltoe.CloudFoundry.Connector.Rabbit;
-using Steeltoe.Extensions.Configuration;
 
 class Worker
 {
-
     private static ConnectionFactory factory;
     private static IConnection connection;
     private static IModel channel;
     private static EventingBasicConsumer consumer;
-    private static bool topologyRecoveryEnabled=true;
+    private static bool enableTopologyRecovery = true;
     private static int msgsReceived = 0;
 
     private const string taskQueueName = "task_queue";
@@ -26,63 +19,61 @@ class Worker
         RabbitMqConsoleEventListener loggingEventSource = new RabbitMqConsoleEventListener();
 
         // Set default interval for heartbeats
+        // Reduce the heartbeat interval so that bad connections are detected sooner than the default of 60s
         ushort heartbeatInterval = 20;
-        string heartbeatIntervalStr = Environment.GetEnvironmentVariable("HEARTBEAT_INTERVAL_SEC");
-        if (heartbeatIntervalStr == null)
+        if (ushort.TryParse(Environment.GetEnvironmentVariable("HEARTBEAT_INTERVAL_SEC"), out heartbeatInterval))
         {
-            Console.WriteLine("HEARTBEAT_INTERVAL_SEC environment variable not defined, using default.");
+            Console.WriteLine("Setting heartbeat interval from HEARTBEAT_INTERVAL_SEC");
         }
         else
         {
-            heartbeatInterval = Convert.ToUInt16(heartbeatIntervalStr);
+            Console.WriteLine("HEARTBEAT_INTERVAL_SEC environment variable not defined, using default.");
+            heartbeatInterval = 20;
         }
-
         Console.WriteLine("Setting heartbeat interval to {0} s", heartbeatInterval);
 
         int port = 5672;
-        string portStr = Environment.GetEnvironmentVariable("RABBITMQ_NODE_PORT");
-        if (portStr != null) {
-            port = Convert.ToInt32(portStr);
+        if (int.TryParse(Environment.GetEnvironmentVariable("RABBITMQ_NODE_PORT"), out port))
+        {
+            Console.WriteLine("Setting port from RABBITMQ_NODE_PORT");
         }
-
-        factory = new ConnectionFactory() { HostName = "localhost", Port = port };
-        if (Environment.GetEnvironmentVariable("VCAP_SERVICES") != null) {
-            // Running on PCF
-            IServiceCollection services = new ServiceCollection();
-            var config = new ConfigurationBuilder()
-                .AddEnvironmentVariables()
-                .AddCloudFoundry()
-                .Build();
-            services.AddRabbitConnection(config);
-            factory = services.BuildServiceProvider().GetService<ConnectionFactory>();
+        else
+        {
+            port = 5672;
         }
+        Console.WriteLine("Port: {0}", port);
 
-        // No need to explicitly set this value, default is already true
-        // factory.AutomaticRecoveryEnabled = true;
+        factory = new ConnectionFactory()
+        {
+            HostName = "shostakovich",
+            UserName = "guest",
+            Password = "guest",
+            Port = port,
+            AutomaticRecoveryEnabled = true,
+            RequestedHeartbeat = heartbeatInterval
+        };
 
         // Since we have a durable queue anyways, there should be no need to recreate it on a connection failure.
         // Otherwise this currently can result in exceptions (if the durable queue home node is down), that
         // hangs the RMQ client.
-        String topologyRecoveryEnabledStr = Environment.GetEnvironmentVariable("TOPOLOGY_RECOVERY_ENABLED");
-        if (topologyRecoveryEnabledStr != null)
+        if (bool.TryParse(Environment.GetEnvironmentVariable("TOPOLOGY_RECOVERY_ENABLED"), out enableTopologyRecovery))
         {
-            if (topologyRecoveryEnabledStr.ToUpper().Equals("FALSE"))
-            {
-                Console.WriteLine("Disabling topology recovery.");
-                factory.TopologyRecoveryEnabled = false;
-                topologyRecoveryEnabled = false;
-            }
+            Console.WriteLine("Setting topology recovery from TOPOLOGY_RECOVERY_ENABLED");
         }
+        else
+        {
+            enableTopologyRecovery = true;
+        }
+        factory.TopologyRecoveryEnabled = enableTopologyRecovery;
+        Console.WriteLine("Topology recovery enabled: {0}", factory.TopologyRecoveryEnabled);
 
-        // Reduce the heartbeat interval so that bad connections are detected sooner than the default of 60s
-        factory.RequestedHeartbeat = heartbeatInterval;
-        using (connection = factory.CreateConnection()) {
-
+        using (connection = factory.CreateConnection())
+        {
             // An autorecovery occurred
             connection.RecoverySucceeded += (sender, e) =>
             {
                 // If topology recovery is not enabled then we'll need to restart the consumer
-                if (!topologyRecoveryEnabled)
+                if (consumer.IsRunning == false)
                 {
                     CreateConsumer(taskQueueName);
                 }
@@ -98,17 +89,20 @@ class Worker
                 channel.CallbackException += (sender, e) => Console.WriteLine(e.Exception);
                 channel.ModelShutdown += (sender, e) => Console.WriteLine($"Channel closed: {e.Cause?.ToString()}");
 
-                while (true)
-                {
-                    Thread.Sleep(1000);
-                }
+                Console.WriteLine("Hit enter key to exit!");
+                Console.ReadLine();
             }
-
-        };
+        }
     }
 
-
-    protected static void CreateConsumer(string queueName) {
+    private static void CreateConsumer(string queueName)
+    {
+        if (consumer != null)
+        {
+            Console.WriteLine("Cancelling consumer with tag: {0}", consumer.ConsumerTag);
+            consumer.Model.BasicCancel(consumer.ConsumerTag);
+            consumer = null;
+        }
 
         consumer = new EventingBasicConsumer(channel);
 
