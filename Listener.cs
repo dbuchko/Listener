2ad60dd6769a1da38c05c101465d5e0afcc15cb3
Luke Bakken <luke@bakken.io>
2018-01-25 10:58:50 -0800

    Threading changes

--- Listener.cs
+++ Listener.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using RabbitMQ.Client.Logging;
@@ -9,6 +10,8 @@ class Worker
     private static IConnection connection;
     private static IModel channel;
     private static EventingBasicConsumer consumer;
+    private static readonly ReaderWriterLockSlim consumerLock = new ReaderWriterLockSlim();
+    private static readonly ManualResetEventSlim consumerRegistered = new ManualResetEventSlim();
     private static bool enableTopologyRecovery = true;
     private static int msgsReceived = 0;
 
@@ -32,14 +35,14 @@ class Worker
         }
         Console.WriteLine("Setting heartbeat interval to {0} s", heartbeatInterval);
 
-        int port = 5672;
+        int port = 5670;
         if (int.TryParse(Environment.GetEnvironmentVariable("RABBITMQ_NODE_PORT"), out port))
         {
             Console.WriteLine("Setting port from RABBITMQ_NODE_PORT");
         }
         else
         {
-            port = 5672;
+            port = 5670;
         }
         Console.WriteLine("Port: {0}", port);
 
@@ -72,56 +75,121 @@ class Worker
             // An autorecovery occurred
             connection.RecoverySucceeded += (sender, e) =>
             {
-                // If topology recovery is not enabled then we'll need to restart the consumer
-                if (consumer.IsRunning == false)
+                /*
+                 * Note: if consumer.IsRunning is false here, it could just be because the Registered event
+                 * has not yet fired for the recovered consumer
+                 */
+                Console.WriteLine("RecoverySucceeded, now waiting on Registered event - ThreadId: {0}",
+                    Thread.CurrentThread.ManagedThreadId);
+                if (consumerRegistered.Wait(factory.ContinuationTimeout))
                 {
+                    Console.WriteLine("RecoverySucceeded, consumer restarted - Consumer tag: {0} IsRunning: {1}",
+                        consumer.ConsumerTag, consumer.IsRunning);
+                }
+                else
+                {
+                    Console.WriteLine("RecoverySucceeded, consumer did not restart! - Consumer tag: {0}",
+                        consumer.ConsumerTag);
                     CreateConsumer(taskQueueName);
                 }
             };
 
             using (channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: taskQueueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
-
-                Console.WriteLine(" [*] Waiting for messages.");
-
-                CreateConsumer(taskQueueName);
                 channel.CallbackException += (sender, e) => Console.WriteLine(e.Exception);
                 channel.ModelShutdown += (sender, e) => Console.WriteLine($"Channel closed: {e.Cause?.ToString()}");
 
-                Console.WriteLine("Hit enter key to exit!");
-                Console.ReadLine();
+                channel.QueueDeclare(queue: taskQueueName,
+                    durable: true, exclusive: false,
+                    autoDelete: false, arguments: null);
+
+                Console.WriteLine("Creating initial consumer and waiting for Registered...");
+                CreateConsumer(taskQueueName);
+                if (consumerRegistered.Wait(factory.ContinuationTimeout))
+                {
+                    Console.WriteLine("Initial consumer started - Consumer tag: {0} IsRunning: {1}",
+                        consumer.ConsumerTag, consumer.IsRunning);
+                    while (true)
+                    {
+                        consumerLock.EnterReadLock();
+                        try
+                        {
+                            Console.WriteLine("Current consumer tag: {0} IsRunning: {1} ThreadId: {2}",
+                                consumer.ConsumerTag, consumer.IsRunning,
+                                Thread.CurrentThread.ManagedThreadId);
+                        }
+                        finally
+                        {
+                            consumerLock.ExitReadLock();
+                        }
+                        Thread.Sleep(TimeSpan.FromSeconds(5));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Initial consumer did not start!");
+                }
             }
         }
     }
 
     private static void CreateConsumer(string queueName)
     {
-        if (consumer != null)
+        consumerLock.EnterWriteLock();
+        try
         {
-            Console.WriteLine("Cancelling consumer with tag: {0}", consumer.ConsumerTag);
-            consumer.Model.BasicCancel(consumer.ConsumerTag);
-            consumer = null;
+            if (consumer != null)
+            {
+                Console.WriteLine("Cancelling consumer with tag: {0} ThreadId: {1}",
+                    consumer.ConsumerTag, Thread.CurrentThread.ManagedThreadId);
+                consumer.Model.BasicCancel(consumer.ConsumerTag);
+                consumer = null;
+            }
+            consumer = new EventingBasicConsumer(channel);
+            consumerRegistered.Reset();
         }
-
-        consumer = new EventingBasicConsumer(channel);
+        finally
+        {
+            consumerLock.ExitWriteLock();
+        }
 
         consumer.Received += (model, ea) =>
         {
             msgsReceived++;
-            Console.WriteLine("Received {0} messages", msgsReceived);
+            Console.WriteLine("Received {0} messages - Consumer tag: {1} ThreadId: {2}",
+                msgsReceived, ea.ConsumerTag, Thread.CurrentThread.ManagedThreadId);
         };
 
         consumer.Shutdown += (model, ea) =>
         {
-            Console.WriteLine("*** Entering Shutdown ***");
+            Console.WriteLine("Shutdown - Cause: {0} ThreadId: {1}",
+                ea.Cause, Thread.CurrentThread.ManagedThreadId);
+            consumerRegistered.Reset();
         };
 
         consumer.ConsumerCancelled += (model, ea) =>
         {
-            Console.WriteLine("*** Received ConsumerCancelled ***");
+            Console.WriteLine("ConsumerCancelled - Consumer tag: {0} ThreadId: {1}",
+                ea.ConsumerTag, Thread.CurrentThread.ManagedThreadId);
+            consumerRegistered.Reset();
         };
 
+        consumer.Registered += (model, ea) =>
+        {
+            Console.WriteLine("Registered - Consumer tag: {0} IsRunning: {1} ThreadId: {2}",
+                ea.ConsumerTag, consumer.IsRunning, Thread.CurrentThread.ManagedThreadId);
+            consumerRegistered.Set();
+        };
+
+        consumer.Unregistered += (model, ea) =>
+        {
+            Console.WriteLine("Unregistered - Consumer tag: {0} IsRunning: {1} ThreadId: {2}",
+                ea.ConsumerTag, consumer.IsRunning, Thread.CurrentThread.ManagedThreadId);
+            consumerRegistered.Reset();
+        };
+
+        Console.WriteLine("BasicConsume - ThreadId: {0}",
+            Thread.CurrentThread.ManagedThreadId);
         channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
     }
 }
